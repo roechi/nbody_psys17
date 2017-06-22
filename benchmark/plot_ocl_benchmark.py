cb497e2453d5a6c574adf8b1cb360a0df042051c
roechi <msg.remus@gmail.com>
2017-06-22 21:25:07 +0200

    updates ocl script

--- benchmark/plot_ocl_benchmark.py
+++ benchmark/plot_ocl_benchmark.py
@@ -1,40 +1,53 @@
 #!/usr/bin/python
-
+import os
 import sys
+import subprocess
 import numpy
-import matplotlib.pyplot as plot
-import string
+import matplotlib.pyplot as plt
 
 if (len(sys.argv) != 2):
     print "Missing arguments.\nUsage: ./plot_benchmark.py [log_file]"
     sys.exit()
 
-with open(sys.argv[1]) as file:
-    content = file.readlines()
-content = [line.strip() for line in content]
+with open(sys.argv[1]) as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
 
-ocl_runtimes = []
-ocl_stdevs = []
-ocl_wgroup_size = []
+ocl_times=[]
+ocl_stdevs=[]
+num_threads=[]
+num_bodies = 0
+simulation_steps = 0
 
+line_idx = 0
 for line in content:
-    values = line.split(';')
-    ocl_wgroup_size += [int(values[0])]
-    ocl_runtimes += [numpy.mean(eval(values[1]))]
-    ocl_stdevs += [numpy.std(eval(values[1]))]
+    if line_idx == 0:
+        num_bodies = int(line)
+    elif line_idx == 1:
+        simulation_steps = int(line)
+    else:
+        vals = line.split(';')
+        num_threads += [int(vals[0])]
+        ocl_times += [numpy.mean(eval(vals[1]))]
+        ocl_stdevs += [numpy.std(eval(vals[1]))]
+    line_idx = line_idx + 1
 
-fig = plot.figure(figsize=(14, 7))
 
-max_threads = len(ocl_wgroup_size)
 
-ax = plot.subplot()
-ax.set_xlim(ocl_wgroup_size[0], ocl_wgroup_size[max_threads - 1])
-line1 = ax.errorbar(ocl_wgroup_size, ocl_runtimes, yerr=ocl_stdevs, label ='OMP')
-plot.xlabel('size of OCL work group')
-plot.ylabel('simulation speed (ms)')
-plot.title('OCL speedup for increased work group size')
+N=len(content)-2
+ind = [x for x in numpy.arange(N)]
+# ind = num_threads
+fig = plt.figure(figsize=(14, 7))
+ax = plt.subplot()
+line2 = ax.errorbar(ind, ocl_times, yerr=ocl_stdevs, label = 'OpenCL')
+ax.legend(loc='upper left')
+# ax.set_xlim([0, N+1])
+plt.xticks(ind, num_threads)
+plt.xlabel('Number of "Threads"')
+plt.ylabel('Simulation speed (ms)')
+plt.title('OpenCL simulation speed with {} bodies and {} steps'.format(num_bodies, simulation_steps))
 
 # Render, Control + C in command line to close the window
-plot.draw()
-plot.waitforbuttonpress(0)
-plot.close(fig)
+plt.draw()
+plt.waitforbuttonpress(0)
+plt.close(fig)
