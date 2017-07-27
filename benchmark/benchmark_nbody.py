8cdd5d9014bcaffb4a09e72522e1b491f3173173
Jonas Jaszkowic <jonasjaszkowic@googlemail.com>
2017-07-27 12:13:31 +0200

    Cleaned up and improved benchmark scripts

--- benchmark/benchmark_nbody.py
+++ benchmark/benchmark_nbody.py
@@ -1,37 +1,65 @@
 #!/usr/bin/python
 import os
 import time
+import sys
+import math
 
 # Benchmarks the OpenCL and OMP implementation
-# TODO: Check if scripts for generating bodies and executable is present
-# TODO: Build project when executable is not present
-# TODO: Allow script to be exited with CTRL-C (not working because of os.system subcalls)
-# TODO: Set maximum number of body via parameter
-
 current_milli_time = lambda: int(round(time.time() * 1000))
+
+if (len(sys.argv) != 4):
+    print "Missing arguments.\nUsage: {} [max_bodies] [simulation_steps] [repeats]".format(sys.argv[0])
+    sys.exit()
+
+max_bodies = int(sys.argv[1])
+simulation_steps = int(sys.argv[2])
+num_repeats = int(sys.argv[3])
+
+if not (max_bodies != 0 and ((max_bodies & (max_bodies - 1)) == 0)):
+    print "{} is not a power of 2. Please provide num_bodies that is a power of 2.".format(num_bodies)
+    sys.exit()
 
 path_to_body_generation = "../resources/generateRandomGalaxy.py"
 path_to_simulation_exe = "../cmake-build-debug/nbody_psys17"
+path_to_plot_exe = "./plot_benchmark.py"
 
-print 'Benchmarking OMP vs. OpenCL implementation.\n'
+if not os.path.isfile(path_to_body_generation):
+    print "{} does not exist!".format(path_to_body_generation)
+    sys.exit();
+
+if not os.path.isfile(path_to_simulation_exe):
+    print "{} does not exist!".format(path_to_simulation_exe)
+    sys.exit();
+
+if not os.path.isfile(path_to_plot_exe):
+    print "{} does not exist!".format(path_to_plot_exe)
+    sys.exit();
+
+print 'Benchmarking OMP vs. OpenCL vs. Sequential implementation.\n'
 
 tmp_file="tmp.txt"
 out_file="tmp_out.txt"
 log_file='benchmark_nbody_{}.log'.format(current_milli_time())
-num_repeats = 8
 
-num_bodies = [pow(2,x) for x in range(1,11)]
-simulation_steps = 1000;
+num_bodies = [pow(2,x) for x in range(int(math.log(max_bodies,2))+1)]
 
 f = open(log_file, 'w');
 
 for cur_num in num_bodies:
+    seq_times=[]
     omp_times=[]
     ocl_times=[]
+
     generate_bodies_call='{} {} {}'.format(path_to_body_generation, cur_num,tmp_file)
     os.system(generate_bodies_call)
 
     for i in range(0,num_repeats):
+        # Benchmark sequential version (i.e. set number of threads to 1)
+        simulation_call = 'OMP_NUM_THREADS={} {} {} {} {} {}'.format(1, path_to_simulation_exe, tmp_file, simulation_steps, 'omp', out_file)
+        before = current_milli_time()
+        os.system(simulation_call)
+        seq_elapsed = current_milli_time() - before
+
         # Benchmark OMP simulation
         simulation_call = '{} {} {} {} {}'.format(path_to_simulation_exe, tmp_file, simulation_steps, 'omp', out_file)
         before = current_milli_time()
@@ -44,12 +72,13 @@ for cur_num in num_bodies:
         os.system(simulation_call)
         ocl_elapsed = current_milli_time() - before
 
+        seq_times += [seq_elapsed]
         omp_times += [omp_elapsed]
         ocl_times += [ocl_elapsed]
 
-    f.write('{};{};{}\n'.format(cur_num,omp_times,ocl_times))
+    f.write('{};{};{};{}\n'.format(cur_num,seq_times,omp_times,ocl_times))
     f.flush()
-    print 'Done. Simulation with {} bodies and {} steps finished, {} repeats.\nOMP: {}, OpenCL: {}\n'.format(cur_num, simulation_steps, num_repeats, omp_times, ocl_times)
+    print 'Done. Simulation with {} bodies and {} steps finished, {} repeats.\nSequential: {}, OMP: {}, OpenCL: {}\n'.format(cur_num, simulation_steps, num_repeats, seq_times, omp_times, ocl_times)
 
 f.close()
 print 'Benchmarking done. Logfile saved in {}.\n'.format(log_file)
@@ -58,4 +87,4 @@ os.remove(tmp_file)
 os.remove(out_file)
 print 'Cleaned up temporary files.\n'
 
-os.system('./plot_benchmark.py {}'.format(log_file))
+os.system('{} {}'.format(path_to_plot_exe, log_file))
--- benchmark/plot_benchmark.py
+++ benchmark/plot_benchmark.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
 import os
 import sys
 import subprocess
@@ -13,19 +13,33 @@ with open(sys.argv[1]) as f:
     content = f.readlines()
 content = [x.strip() for x in content]
 
+seq_times = []
 omp_times=[]
 ocl_times=[]
+
+seq_stdevs = []
 omp_stdevs=[]
 ocl_stdevs=[]
+
 num_bodies=[]
 
 for line in content:
     vals = line.split(';')
     num_bodies += [int(vals[0])]
-    omp_times += [numpy.mean(eval(vals[1]))]
-    ocl_times += [numpy.mean(eval(vals[2]))]
-    omp_stdevs += [numpy.std(eval(vals[1]))]
-    ocl_stdevs += [numpy.std(eval(vals[2]))]
+
+    seq_times += [numpy.mean(eval(vals[1]))]
+    omp_times += [numpy.mean(eval(vals[2]))]
+    ocl_times += [numpy.mean(eval(vals[3]))]
+
+    seq_stdevs += [numpy.std(eval(vals[1]))]
+    omp_stdevs += [numpy.std(eval(vals[2]))]
+    ocl_stdevs += [numpy.std(eval(vals[3]))]
+
+    if int(vals[0]) == 2048:
+        print "Seq mean={}, Seq std={}".format(numpy.mean(eval(vals[1])), numpy.std(eval(vals[1])))
+        print "OMP mean={}, OMP std={}, Speedup={}".format(numpy.mean(eval(vals[2])), numpy.std(eval(vals[2])), numpy.mean(eval(vals[1])) / numpy.mean(eval(vals[2])))
+        print "OCL mean={}, OCL std={}, Speedup={}".format(numpy.mean(eval(vals[3])), numpy.std(eval(vals[3])), numpy.mean(eval(vals[1])) / numpy.mean(eval(vals[3])))
+
 
 
 N=len(content)
@@ -33,14 +47,16 @@ ind = [x for x in numpy.arange(N)]
 # ind = num_bodies
 fig = plt.figure(figsize=(14, 7))
 ax = plt.subplot()
-line1 = ax.errorbar(ind, omp_times, yerr=omp_stdevs, label = 'OMP')
-line2 = ax.errorbar(ind, ocl_times, yerr=ocl_stdevs, label = 'OpenCL')
+line1 = ax.errorbar(ind, seq_times, yerr=seq_stdevs, label = 'Sequential')
+line2 = ax.errorbar(ind, omp_times, yerr=omp_stdevs, label = 'OMP')
+line3 = ax.errorbar(ind, ocl_times, yerr=ocl_stdevs, label = 'OpenCL')
+
 ax.legend(loc='upper left')
 # ax.set_xlim([0, N+1])
 plt.xticks(ind, num_bodies)
 plt.xlabel('Number of bodies')
 plt.ylabel('Simulation speed (ms)')
-plt.title('OMP vs OpenCL simulation speed')
+plt.title('Sequential vs. OMP vs. OpenCL simulation speed')
 
 # Render, Control + C in command line to close the window
 plt.draw()
